0f9f9f35737f2d63d983d7b6518fa96320df3037
MgSuper <4.2836722e+07+MgSuper@users.noreply.github.com>
2026-09-12 22:46:19 +0000

    feat: integrate Firebase Analytics and Crashlytics

--- lib/main_common.dart
+++ lib/main_common.dart
@@ -1,3 +1,6 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
@@ -8,11 +11,47 @@ import 'package:startup_launch/core/config/environment.dart';
 import 'package:startup_launch/core/di/service_locator.dart';
 import 'package:startup_launch/core/utils/app_bloc_observer.dart';
 import 'package:startup_launch/features/onboarding/data/onboarding_storage.dart';
+import 'package:startup_launch/firebase_options.dart';
+
+bool get _supportsFirebase {
+  if (kIsWeb) return true;
+  return switch (defaultTargetPlatform) {
+    TargetPlatform.android ||
+    TargetPlatform.iOS ||
+    TargetPlatform.macOS ||
+    TargetPlatform.windows => true,
+    _ => false,
+  };
+}
+
+bool get _supportsCrashlytics {
+  if (kIsWeb) return false;
+  return switch (defaultTargetPlatform) {
+    TargetPlatform.android ||
+    TargetPlatform.iOS ||
+    TargetPlatform.macOS => true,
+    _ => false,
+  };
+}
 
 Future<void> bootstrap(Environment env) async {
   final widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
 
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
+  if (_supportsFirebase) {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+  }
+
+  if (_supportsCrashlytics) {
+    FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+    PlatformDispatcher.instance.onError = (error, stack) {
+      FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+      return true;
+    };
+  }
 
   final config = AppConfig.from(env);
 
